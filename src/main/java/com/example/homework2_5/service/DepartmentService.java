3ce47b6875062ac277dc1b4ae0ee84eb38399926
17152 <nazarovtolya2@gmail.com>
2023-05-21 22:15:58 +0500

    ДЗ

--- src/main/java/com/example/homework2_5/service/DepartmentService.java
+++ src/main/java/com/example/homework2_5/service/DepartmentService.java
@@ -4,6 +4,9 @@ import com.example.homework2_5.model.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentService {
@@ -24,5 +27,16 @@ public class DepartmentService {
                 .filter(e->e.getDepartment()==department)
                 .min(Comparator.comparingDouble(Employee::getSalary))
                 .orElse(null);
+    }
+
+    public List<Employee> getEmployeeByDepartment(int department) {
+        return employeeService.getAll().stream()
+                .filter(e->e.getDepartment()==department)
+                .collect(Collectors.toList());
+    }
+
+    public Map <Integer, List <Employee>> getEmployeeGrouped() {
+        return employeeService.getAll().stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
     }
 }
